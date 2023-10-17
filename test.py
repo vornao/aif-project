3ff770ab496e7ec73bdacd6a9c562f6413b2b164
irenedovichi <irenedovichi@gmail.com>
2023-10-17 22:55:47 +0200

    another step

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ env = gym.make("MiniHack-River-v0")
 env.reset() # each reset generates a new environment instance
 env.step(1)  # move agent '@' north
 env.render()
+env.step(1)  # move agent '@' north
+env.render()
